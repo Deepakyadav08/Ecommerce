7c78e7d8abeb4508b3c92a3ef64b5236ac5e342a
Deepak Yadav <youremail@example.com>
2026-05-14 17:25:21 +0530

    Fixed static files settings

--- .history/config/settings_20260514172407.py
+++ .history/config/settings_20260514172407.py
@@ -118,12 +118,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-import os
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
